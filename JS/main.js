c1733319fa1bc884cd25be097473b0abef8e9f16
Dev1 - filip <pelloiafilippo@live.com>
2023-07-28 22:03:48 +0200

    first commit 28 july

--- JS/main.js
+++ JS/main.js
@@ -2,9 +2,7 @@
 // ============ DROPDOWN MENU
 
 var dropdown = document.getElementById("navbarDropdown");
-
 var dropdownMenu = document.getElementById("navbarDropdown").nextElementSibling;
-
 var timeoutId;
 
 dropdown.addEventListener("mouseenter", function () {
@@ -84,11 +82,11 @@ document.addEventListener("scroll", function () {
 fetch('dataProduct.json')
   .then(response => response.json())
   .then(data => {
-    const resultsContainer = document.querySelector('.product-collection .d-flex');
+    const resultsContainer = document.querySelector('.product-collection .d-flex'); //IDENTIFICO IL LUOGO FINALE
 
-    data.forEach(item => {
-      const resultElement = document.createElement('div');
-      resultElement.classList.add('result', 'col-lg-3', 'col-6');
+    data.forEach(item => {                              //PER OGNI ELEMENTO
+      const resultElement = document.createElement('div');  //CREO DIV
+      resultElement.classList.add('result', 'col-lg-3', 'col-6');  //ASSEGNO CLASSI
 
       const html = `
         <div class="data-container mx-2">
@@ -167,6 +165,79 @@ fetch('dataProduct.json')
 
 
 // ============ CHECKBOX
+// fetch('dataProduct.json')
+//   .then(response => response.json())
+//   .then(data => {
+//     const resultsContainer = document.querySelector('.product-collection .d-flex');
+//     const checkboxes = document.querySelectorAll('.form-check-input');
+//     const oggetti = data;
+    
+//     // Inizializza un oggetto vuoto per memorizzare lo stato delle checkbox selezionate
+//     const checkboxState = {};
+
+//     // Inizializza gli stati delle checkbox con false
+//     checkboxes.forEach((checkbox) => {
+//       checkboxState[checkbox.value] = false;
+//     });
+
+//     // Variabile per memorizzare i risultati filtrati
+//     let filteredData = oggetti;
+
+//     function filterItems() {
+//       const selezionati = Object.keys(checkboxState).filter((key) => checkboxState[key]);
+
+//       filteredData = oggetti.filter((oggetto) => {
+//         const { description, category, finiture } = oggetto;
+//         const descriptionLower = description.toLowerCase();
+
+//         return (
+//           selezionati.length === 0 || 
+//           selezionati.some((valore) => descriptionLower.includes(valore)) ||
+//           selezionati.some((valore) => category.includes(valore)) ||
+//           selezionati.some((valore) => finiture.includes(valore))
+//         );
+//       });
+
+//       displayFilteredData();
+//     }
+
+//     function displayFilteredData() {
+//       resultsContainer.innerHTML = '';
+//       filteredData.forEach((item) => {
+//         const resultElement = document.createElement('div');
+//         resultElement.classList.add('result', 'col-lg-3');
+
+//         const html = `
+//           <div class="data-container mx-2">
+//             <div class="image-container d-flex justify-content-center align-items-center mt-5">
+//               <img class="image-result" src="${item.image}" alt="${item.name}">
+//             </div>
+//             <div class="result-name text-center mt-3">${item.name}</div>
+//             <p class="result-description text-center px-3">${item.description}</p>
+//           </div>
+//         `;
+
+//         resultElement.innerHTML = html;
+//         resultsContainer.appendChild(resultElement);
+//       });
+//     }
+
+//     // Aggiungi l'event listener solo una volta
+//     checkboxes.forEach((checkbox) => {
+//       checkbox.addEventListener('change', function() {
+//         checkboxState[checkbox.value] = checkbox.checked;
+//         filterItems();
+//       });
+//     });
+//   })
+//   .catch(error => {
+//     console.error('Si è verificato un errore durante il caricamento del file JSON:', error);
+//   });
+
+
+
+
+
 
 fetch('dataProduct.json')
   .then(response => response.json())
@@ -175,42 +246,73 @@ fetch('dataProduct.json')
     const checkboxes = document.querySelectorAll('.form-check-input');
     const oggetti = data;
 
-    function filterItems() {
-      const selezionati = Array.from(checkboxes).filter((checkbox) => checkbox.checked).map((checkbox) => checkbox.value);
 
-      const oggettiFiltrati = oggetti.filter(oggetto => {
-        const { description, category, finiture } = oggetto;
-        const descriptionLower = description.toLowerCase();
+// Inizializza un oggetto vuoto per memorizzare lo stato delle checkbox selezionate
+const checkboxState = {
+  tipo: {},
+  categorie: {},
+  finiture: {},
+};
 
-        return (
-          selezionati.length === 0 ||  selezionati.some((valore) => descriptionLower.includes(valore)) || selezionati.some(valore => category.includes(valore)) || selezionati.some(valore => finiture.includes(valore))
-        );
-      });
+// Inizializza gli stati delle checkbox con false
+document.querySelectorAll('.form-check-input').forEach((checkbox) => {
+  checkboxState[checkbox.dataset.group][checkbox.value] = false;
+});
 
-      resultsContainer.innerHTML = '';
-      oggettiFiltrati.forEach((item) => {
-        const resultElement = document.createElement('div');
-        resultElement.classList.add('result', 'col-lg-3');
+// Variabile per memorizzare i risultati filtrati
+let filteredData = oggetti;
 
-        const html = `
-          <div class="data-container mx-2">
-            <div class="image-container d-flex justify-content-center align-items-center mt-5">
-              <img class="image-result" src="${item.image}" alt="${item.name}">
-            </div>
-            <div class="result-name text-center mt-3">${item.name}</div>
-            <p class="result-description text-center px-3">${item.description}</p>
-          </div>
-        `;
+function filterItems() {
+  const selectedTipo = Object.keys(checkboxState.tipo).filter((key) => checkboxState.tipo[key]);
+  const selectedCategoria = Object.keys(checkboxState.categorie).filter((key) => checkboxState.categorie[key]);
+  const selectedFiniture = Object.keys(checkboxState.finiture).filter((key) => checkboxState.finiture[key]);
 
-        resultElement.innerHTML = html;
-        resultsContainer.appendChild(resultElement);
-      });
-    }
+  filteredData = oggetti.filter((oggetto) => {
+    const { description, category, finiture } = oggetto;
+    const descriptionLower = description.toLowerCase();
 
-    checkboxes.forEach((checkbox) => {
-      checkbox.addEventListener('change', filterItems);
-    });
-  })
-  .catch(error => {
-    console.error('Si è verificato un errore durante il caricamento del file JSON:', error);
+    return (
+      (selectedTipo.length === 0 || selectedTipo.some((valore) => descriptionLower.includes(valore))) &&
+      (selectedCategoria.length === 0 || selectedCategoria.includes(category)) &&
+      (selectedFiniture.length === 0 || selectedFiniture.some((valore) => finiture.includes(valore)))
+    );
   });
+
+  displayFilteredData();
+}
+
+
+
+function displayFilteredData() {
+    resultsContainer.innerHTML = '';
+    filteredData.forEach((item) => {
+      const resultElement = document.createElement('div');
+      resultElement.classList.add('result', 'col-lg-3');
+
+      const html = `
+        <div class="data-container mx-2">
+          <div class="image-container d-flex justify-content-center align-items-center mt-5">
+            <img class="image-result" src="${item.image}" alt="${item.name}">
+          </div>
+          <div class="result-name text-center mt-3">${item.name}</div>
+          <p class="result-description text-center px-3">${item.description}</p>
+        </div>
+      `;
+
+      resultElement.innerHTML = html;
+      resultsContainer.appendChild(resultElement);
+    });
+  }
+
+
+
+// Aggiungi l'event listener solo una volta
+document.querySelectorAll('.form-check-input').forEach((checkbox) => {
+  checkbox.addEventListener('change', function () {
+    checkboxState[checkbox.dataset.group][checkbox.value] = checkbox.checked;
+    filterItems();
+  });
+});
+
+});
+
